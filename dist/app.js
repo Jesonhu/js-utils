a501b9d1c970847fc382e71c231e81e973f7e5e2
Jeson <jesonhu_web@163.com>
2018-07-27 18:16:38 +0800

    feat: 添加 lodash 使用

--- dist/app.js
+++ dist/app.js
@@ -119,6 +119,17 @@ eval("/** 时间处理功能函数 */\r\n// const getNowData = require('./getNow
 
 /***/ }),
 
+/***/ "./src/document/index.js":
+/*!*******************************!*\
+  !*** ./src/document/index.js ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("/**\r\n * 页面属性设置对象\r\n */\r\nconst Doc = {\r\n\r\n  /**\r\n   * `rem` 单位设置.\r\n   * \r\n   * `window.html` 设置 `font-size` 的大小\r\n   * \r\n   * @param {number} designWidth UI设计图片的宽度\r\n   * @param {number} unit 单位\r\n   * \r\n   * @desc setRem(640) => `width:640px` `font-size:50px;`\r\n   * @desc setRem(750) => `width:750px` `font-size:50px;`\r\n   */\r\n  setRem(designWidth = 750, unit = 100) {\r\n    (function(doc, win, dWid = 750) {\r\n      const docEl = doc.documentElement,\r\n          resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',\r\n          recalc = function() {\r\n              let clientWidth = docEl.clientWidth;\r\n              if (!clientWidth) {\r\n                return false;\r\n              } else if (clientWidth > designWidth) {\r\n                clientWidth = dWid;\r\n              }\r\n              docEl.style.fontSize = unit * (clientWidth / dWid) + 'px';\r\n          };\r\n      if (!doc.addEventListener) return;\r\n      win.addEventListener(resizeEvt, recalc, false);\r\n      doc.addEventListener('DOMContentLoaded', recalc, false);\r\n    })(document, window, designWidth, unit);\r\n  }\r\n}\r\n\r\nmodule.exports = Doc;\n\n//# sourceURL=webpack:///./src/document/index.js?");
+
+/***/ }),
+
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
@@ -126,7 +137,29 @@ eval("/** 时间处理功能函数 */\r\n// const getNowData = require('./getNow
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("// import EventDispatcher from './must';\r\nconst Date = __webpack_require__(/*! ./date */ \"./src/date/index.js\");\r\n\r\n// 期待使用方式\r\n// 浏览器中：\r\n// new window.cTools.Time() // 返回实际处理类\r\n\r\n/**\r\n * `JavaScript` 常用工具集合\r\n */\r\nconst cTools = {\r\n  Date: Date\r\n}\r\n\r\n\r\n// 导出处理\r\n// ??? webpack 浏览器环境不执行\r\n// @see [jQuery方式](https://gitee.com/weblife/jquery-code-understand/blob/master/src/slim/01_%E6%A8%A1%E5%9D%97%E4%B8%8E%E6%B5%8F%E8%A7%88%E5%99%A8%E5%AF%BC%E5%87%BA%E5%A4%84%E7%90%86.js)\r\n// 支持 CommonJS 方式导出\r\n// if (typeof module === 'object' && typeof module.exports === 'object') {\r\n//   module.exports = cTools;\r\n// } else { // 浏览器环境\r\n//   window.cTools = cTools;\r\n// }\r\n\r\nwindow.cTools = cTools;\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("// import EventDispatcher from './must';\r\n\r\n/** 日期管理类 */\r\nconst Date = __webpack_require__(/*! ./date */ \"./src/date/index.js\");\r\n/** `document`下相关属性设置对象 */\r\nconst Doc = __webpack_require__(/*! ./document */ \"./src/document/index.js\");\r\n/** `window`相关设置对象 */\r\nconst Win = __webpack_require__(/*! ./window */ \"./src/window/index.js\");\r\n\r\n/**\r\n * `JavaScript` 常用工具集合对象\r\n */\r\nconst cTools = {\r\n  Date: Date,\r\n  Doc: Doc,\r\n  Win: Win\r\n}\r\n\r\n\r\n// 导出处理\r\n// ??? webpack 浏览器环境不执行\r\n// @see [jQuery方式](https://gitee.com/weblife/jquery-code-understand/blob/master/src/slim/01_%E6%A8%A1%E5%9D%97%E4%B8%8E%E6%B5%8F%E8%A7%88%E5%99%A8%E5%AF%BC%E5%87%BA%E5%A4%84%E7%90%86.js)\r\n// 支持 CommonJS 方式导出\r\n// if (typeof module === 'object' && typeof module.exports === 'object') {\r\n//   module.exports = cTools;\r\n// } else { // 浏览器环境\r\n//   window.cTools = cTools;\r\n// }\r\n\r\nwindow.cTools = cTools;\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/window/index.js":
+/*!*****************************!*\
+  !*** ./src/window/index.js ***!
+  \*****************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("const localStorage = __webpack_require__(/*! ./localStorage */ \"./src/window/localStorage.js\");\r\n\r\nconst Win = {\r\n  localStorage: localStorage\r\n}\r\nmodule.exports = Win;\n\n//# sourceURL=webpack:///./src/window/index.js?");
+
+/***/ }),
+
+/***/ "./src/window/localStorage.js":
+/*!************************************!*\
+  !*** ./src/window/localStorage.js ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("/**\r\n * 本地存储功能封装.\r\n * \r\n * @attr {Fn} getLocal 获取本地存储的值\r\n * @attr {Fn} setLocal 设置本地存储的值\r\n * @attr {Fn} removeLocal 删除本地存储的某个值\r\n */\r\nconst STORAGE_USER_KEY = 'STORAGE_USER_KEY'\r\n\r\nconst localStorage = {\r\n  /**\r\n   * 获取指定键的本地存储值.\r\n   * \r\n   * @param {string} key 如果不填此参数，默认本地存储的键 'STORAGE_USER_KEY'\r\n   * @return {JSON} 返回一个有本地存储值构成的对象 \r\n   */\r\n  getLocal (key = STORAGE_USER_KEY) {\r\n    console.log('get local opeartion');\r\n    return JSON.parse(window.localStorage.getItem(key));\r\n  },\r\n\r\n  /**\r\n   * 设置指定键的本地存储值.\r\n   * \r\n   * @param {string} key 本地存储的键,可\r\n   * @param {any} res 对应键的值\r\n   * @param {boolean} isSaveOldData 可选，判断是增加值还是替换为新的值(false：替换)\r\n   * \r\n   * @desc 没有第三个参数或者第三个参数为false,则覆盖本地存储的该条记录\r\n   * @desc 第三个参数为true,则添加一个字段进去\r\n   */\r\n  setLocal (key, res, isAddData = false) {\r\n\r\n    // `key` 为`字符串`并且有非``的时候才作为存储的`键`使用\r\n    // 不为字符串，就使用默认的键`STORAGE_USER_KEY`\r\n    if (typeof key !== 'string' && key.length > 0) {\r\n      key = STORAGE_USER_KEY;\r\n    }\r\n    \r\n    // 增加或者内容替换为新的值\r\n    // 如果值`冲突`就使用最新的值\r\n    // 如果值`新增`就添加到本地存储中--功能未完成\r\n    if (isAddData) {\r\n      let oldData = this.getLocal(key);\r\n      console.log(typeof oldData);\r\n      const newData = JSON.stringify(oldData.concat(res));\r\n      // return window.localStorage.setItem(key, newData);\r\n      window.localStorage.setItem(key, newData);\r\n    }\r\n    console.log('set local opeartion');\r\n    // return window.localStorage.setItem(key, JSON.stringify(res))\r\n    window.localStorage.setItem(key, JSON.stringify(res))\r\n  },\r\n\r\n  /**\r\n   * 清除本地存储\r\n   * @param {string} key 可选，默认本地存储的键 'STORAGE_USER_KEY'\r\n   */\r\n  removeLocal (key = STORAGE_USER_KEY) {\r\n    console.log('remove local opeartion');\r\n    window.localStorage.removeItem(key);\r\n  }\r\n}\r\n\r\nmodule.exports = localStorage;\n\n//# sourceURL=webpack:///./src/window/localStorage.js?");
 
 /***/ })
 
---
+++ src/common/obj.js
@@ -0,0 +1,17 @@
+/**
+ * 对象的常用功能 
+ */
+const obj = {
+  /**
+   * 判断两个对象是否相等.
+   * 
+   * @param {any} a 对象1
+   * @param {any} b 对象2
+   * @return {boolean} 相等: `true`; 不等: `false` 
+   */
+  isEqual(a, b) {
+
+  }
+}
+
+module.exports = obj;
